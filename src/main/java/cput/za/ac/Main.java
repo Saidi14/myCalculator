bbf66563fd246dee374dc9fef9f8386f17c6365d
Saidi <sparkinshot@gmail.com>
2024-03-23 04:53:17 +0200

    added a basic calculator gui

--- src/main/java/cput/za/ac/Main.java
+++ src/main/java/cput/za/ac/Main.java
@@ -2,6 +2,6 @@ package cput.za.ac;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorGui init = new CalculatorGui();
     }
 }
